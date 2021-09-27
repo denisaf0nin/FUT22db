97d0ffaf4766748ca47d660a18ca91ecbe62b3fc
denisaf0nin <59995500+denisaf0nin@users.noreply.github.com>
2021-09-27 16:09:13 +0300

    initial

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Access denied on page 89. Try running with time.sleep and custom headers
 
 import requests
 from bs4 import BeautifulSoup
